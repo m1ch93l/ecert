9f96254480df88dbccf5566f1f732b08029be5cd
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-09-07 20:19:24 +0800

    Integrate jquery and datatables.

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -13,4 +13,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
+    <!-- jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <!-- datatables -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.5/css/dataTables.dataTables.css" />
+
+    <script src="https://cdn.datatables.net/2.1.5/js/dataTables.js"></script>
 </head>
