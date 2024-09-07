5dde969fad565684155b0c84c65ec23b5f1ded43
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-09-07 20:44:21 +0800

    Use extendable row data.

--- admin/home.php
+++ admin/home.php
@@ -8,18 +8,66 @@
     <main>
         <div class="container-md">
             <div class="row">
-                <div class="col-sm-12">
-                    <h1 class="text-center position-absolute my-5">
-                        
-                    </h1>
-                    <div id="printDiv">
-                        <img src="../1.png" class="img-fluid" alt="certificate">
+                <?php
+                $sql   = "SELECT * FROM participant";
+                $query = $conn->query($sql);
+                while ($row = $query->fetch_assoc()) {
+                    ?>
+                    <div class="col-sm-12">
+                        <table id="student" class="table table-striped" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th class="text-start">USN</th>
+                                    <th class="text-start">Full Name</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $sql   = "SELECT * FROM participant";
+                                $query = $conn->query($sql);
+                                while ($row = $query->fetch_assoc()) {
+                                    ?>
+                                    <tr>
+                                        <td class="text-start">
+                                            <div class="d-flex justify-content-between">
+                                                <?php echo $row["participant_id"]; ?>
+                                                <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="collapse" data-bs-target="#<?php echo $row["id"]; ?>" aria-expanded="false" aria-controls="<?php echo $row["id"]; ?>">
+                                                    Details
+                                                </button>
+                                            </div>
+                                        </td>
+                                        <td class="text-start"><?php echo $row["fullname"]; ?></td>
+                                        <td>
+                                            <div class="collapse" id="<?php echo $row["id"]; ?>">
+                                                <div class="card card-body">
+                                                    <p>USN: <?php echo $row["participant_id"]; ?></p>
+                                                    <p>Full Name: <?php echo $row["fullname"]; ?></p>
+                                                    <p>Email: <?php echo $row["email"]; ?></p>
+                                                    <p>Phone: <?php echo $row["phone"]; ?></p>
+                                                    <p>Address: <?php echo $row["address"]; ?></p>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                }
+                                ?>
+                            </tbody>
+                        </table>
                     </div>
-                    <button onclick="printLetterSize()" class="btn btn-primary">Print</button>
-                </div>
+                    <?php
+                }
+                ?>
             </div>
         </div>
     </main>
 </body>
+
+<script>
+    $(document).ready(function () {
+        $('#student').DataTable();
+    });
+</script>
 
 </html>
