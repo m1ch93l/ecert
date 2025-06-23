19cd40ecc7ee08a05cc067e8300a8b8e4934bf4e
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-06-23 23:05:52 +0800

    remove toast delay

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if (isset($_SESSION['participant'])) {
     <?php if (isset($_SESSION['error'])) : ?>
         <div aria-live="polite" aria-atomic="true" class="position-relative">
             <div class="toast-container position-fixed top-0 end-0 p-3">
-                <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="10000">
+                <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
                     <div class="toast-header">
                         <img src="..." class="rounded me-2" alt="...">
                         <strong class="me-auto">Bootstrap</strong>
