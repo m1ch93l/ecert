1019bf1836ae1d699b876e1110fbf397e8dea7aa
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2025-07-11 18:01:27 +0800

    Improved security.

--- admin/home.php
+++ admin/home.php
@@ -30,129 +30,125 @@
     <main>
         <div class="container-md">
             <div class="row">
-                <?php
-                $sql   = "SELECT * FROM participant";
-                $query = $conn->query($sql);
-                while ($row = $query->fetch_assoc()) {
-                    ?>
-                    <div class="col-sm-12">
-                        <div class="table-responsive">
-                            <table id="student" class="table table-striped" style="width:100%">
-                                <thead>
+                <div class="col-sm-12">
+                    <div class="table-responsive">
+                        <table id="student" class="table table-striped" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th class="text-start">USN</th>
+                                    <th class="text-start">Full Name</th>
+                                    <th class="text-start">Certificates</th>
+                                    <th>Events</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $stmt   = $conn->prepare("SELECT * FROM participant");
+                                $stmt->execute();
+                                $result = $stmt->get_result();
+                                while ($row = $result->fetch_assoc()) {
+                                    ?>
                                     <tr>
-                                        <th class="text-start">USN</th>
-                                        <th class="text-start">Full Name</th>
-                                        <th class="text-start">Certificates</th>
-                                        <th>Events</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                    $sql   = "SELECT * FROM participant";
-                                    $query = $conn->query($sql);
-                                    while ($row = $query->fetch_assoc()) {
-                                        ?>
-                                        <tr>
-                                            <td class="text-start">
-                                                <div class="d-flex justify-content-between">
-                                                    <?php echo $row["participant_id"]; ?>
-                                                    <button type="button" class="btn btn-primary btn-sm"
-                                                        data-bs-toggle="collapse" data-bs-target="#<?php echo $row["id"]; ?>"
-                                                        aria-expanded="false" aria-controls="<?php echo $row["id"]; ?>">
-                                                        Details
-                                                    </button>
-                                                </div>
-                                            </td>
-                                            <td class="text-start text-capitalize"><?php echo $row["fullname"]; ?></td>
-                                            <td class="text-start">
-                                                <?php
-                                                $sql1   = "SELECT COUNT(*) as count FROM acquired_cert WHERE participant_id = '" . $row["id"] . "'";
-                                                $query1 = $conn->query($sql1);
-                                                $row1   = $query1->fetch_assoc();
-                                                echo $row1["count"];
-                                                ?>
-                                            </td>
-                                            <td>
-                                                <div class="collapse" id="<?php echo $row["id"]; ?>">
-                                                    <div class="card card-body">
-                                                        <?php
-                                                        $sql1   = "SELECT * FROM acquired_cert JOIN certificate ON acquired_cert.certificate_id = certificate.id WHERE participant_id = '" . $row["id"] . "'";
-                                                        $query1 = $conn->query($sql1);
-                                                        while ($row1 = $query1->fetch_assoc()) {
-                                                            ?>
-                                                            <div><?= $row1["event"]; ?></div><br>
-                                                            <?php
-                                                        }
+                                        <td class="text-start">
+                                            <div class="d-flex justify-content-between">
+                                                <?php echo $row["participant_id"]; ?>
+                                                <button type="button" class="btn btn-primary btn-sm"
+                                                    data-bs-toggle="collapse" data-bs-target="#<?php echo $row["id"]; ?>"
+                                                    aria-expanded="false" aria-controls="<?php echo $row["id"]; ?>">
+                                                    Details
+                                                </button>
+                                            </div>
+                                        </td>
+                                        <td class="text-start text-capitalize"><?php echo $row["fullname"]; ?></td>
+                                        <td class="text-start">
+                                            <?php
+                                            $stmt2   = $conn->prepare("SELECT COUNT(*) as count FROM acquired_cert WHERE participant_id = ?");
+                                            $stmt2->bind_param("i", $row["id"]);
+                                            $stmt2->execute();
+                                            $result2 = $stmt2->get_result();
+                                            $row2    = $result2->fetch_assoc();
+                                            echo $row2["count"];
+                                            ?>
+                                        </td>
+                                        <td>
+                                            <div class="collapse" id="<?php echo $row["id"]; ?>">
+                                                <div class="card card-body">
+                                                    <?php
+                                                    $stmt3   = $conn->prepare("SELECT * FROM acquired_cert JOIN certificate ON acquired_cert.certificate_id = certificate.id WHERE participant_id = ?");
+                                                    $stmt3->bind_param("i", $row["id"]);
+                                                    $stmt3->execute();
+                                                    $result3 = $stmt3->get_result();
+                                                    while ($row3 = $result3->fetch_assoc()) {
                                                         ?>
-                                                    </div>
+                                                        <div><?= $row3["event"]; ?></div><br>
+                                                        <?php
+                                                    }
+                                                    ?>
                                                 </div>
-                                            </td>
-                                            <td>
-                                                <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal"
-                                                    data-bs-target="#modal-<?php echo $row["id"]; ?>">Add Cert</button>
-                                                <div class="modal fade" id="modal-<?php echo $row["id"]; ?>" tabindex="-1"
-                                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                    <div class="modal-dialog">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header">
-                                                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                                    aria-label="Close"></button>
-                                                            </div>
-                                                            <div class="modal-body">
-                                                                <form action="add-cert" method="post">
-                                                                    <div class="mb-3">
-                                                                        <label for="participant_id"
-                                                                            class="form-label">Participant
-                                                                            ID</label>
-                                                                        <input type="hidden" class="form-control"
-                                                                            id="participant_id" name="participant_id"
-                                                                            value="<?php echo $row["id"]; ?>">
-                                                                    </div>
-                                                                    <div class="mb-3">
-                                                                        <label for="certificate_id"
-                                                                            class="form-label">Certificate
-                                                                            ID</label>
-                                                                        <input type="hidden" name="participant_id"
-                                                                            value="<?php echo $row["id"]; ?>">
-                                                                        <select class="form-select" id="certificate_id"
-                                                                            name="certificate_id">
-                                                                            <?php
-                                                                            $sql2   = "SELECT * FROM certificate";
-                                                                            $query2 = $conn->query($sql2);
-                                                                            while ($row2 = $query2->fetch_assoc()) {
-                                                                                ?>
-                                                                                <option value="<?php echo $row2["id"]; ?>">
-                                                                                    <?php echo $row2["event"]; ?>
-                                                                                </option>
-                                                                                <?php
-                                                                            }
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal"
+                                                data-bs-target="#modal-<?php echo $row["id"]; ?>">Add Cert</button>
+                                            <div class="modal fade" id="modal-<?php echo $row["id"]; ?>" tabindex="-1"
+                                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                                aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <form action="add-cert" method="post">
+                                                                <div class="mb-3">
+                                                                    <label for="participant_id"
+                                                                        class="form-label">Participant
+                                                                        ID</label>
+                                                                    <input type="hidden" class="form-control"
+                                                                        id="participant_id" name="participant_id"
+                                                                        value="<?php echo $row["id"]; ?>">
+                                                                </div>
+                                                                <div class="mb-3">
+                                                                    <label for="certificate_id"
+                                                                        class="form-label">Certificate
+                                                                        ID</label>
+                                                                    <select class="form-select" id="certificate_id"
+                                                                        name="certificate_id">
+                                                                        <?php
+                                                                        $stmt4   = $conn->prepare("SELECT * FROM certificate");
+                                                                        $stmt4->execute();
+                                                                        $result4 = $stmt4->get_result();
+                                                                        while ($row4 = $result4->fetch_assoc()) {
                                                                             ?>
-                                                                        </select>
-                                                                    </div>
-                                                                    <button type="submit" class="btn btn-primary">Add</button>
-                                                                </form>
-                                                            </div>
-                                                            <div class="modal-footer">
-                                                                <button type="button" class="btn btn-secondary"
-                                                                    data-bs-dismiss="modal">Close</button>
-                                                            </div>
+                                                                            <option value="<?php echo $row4["id"]; ?>">
+                                                                                <?php echo $row4["event"]; ?>
+                                                                            </option>
+                                                                            <?php
+                                                                        }
+                                                                        ?>
+                                                                    </select>
+                                                                </div>
+                                                                <button type="submit" class="btn btn-primary">Add</button>
+                                                            </form>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary"
+                                                                data-bs-dismiss="modal">Close</button>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </td>
-                                        </tr>
-                                        <?php
-                                    }
-                                    ?>
-                                </tbody>
-                            </table>
-                        </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                }
+                                ?>
+                            </tbody>
+                        </table>
                     </div>
-                    <?php
-                }
-                ?>
+                </div>
             </div>
         </div>
     </main>
@@ -165,3 +161,4 @@
 </script>
 
 </html>
+
